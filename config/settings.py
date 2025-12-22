75cfbc979f8f94e9eda378a80a9c5d2be64a9fd4
sulimk <sulim.koysultanov13@gmail.com>
2025-12-22 22:32:59 +0300

    python

--- config/settings.py
+++ config/settings.py
@@ -4,4 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+# BOT_TOKEN = os.getenv('BOT_TOKEN')
+
+BOT_TOKEN = '8201925034:AAESmCaHvi0lm0djfxxSNEuZzLnjDHMhtbM'
